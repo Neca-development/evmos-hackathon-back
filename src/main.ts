3025f8c57dc6ed047c8ffe6a0d8f5444b0d37ad5
Aleksey <klimenkoaleksey02@gmail.com>
2022-08-25 17:29:00 +0300

    feat: try to swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -9,20 +8,17 @@ async function bootstrap() {
 
   const config: ApiConfigService = app.get(ApiConfigService);
   const { port } = config;
+  app.setGlobalPrefix('api');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Unistory nestJS starter')
+    .setDescription('Unistory nestJS starter API')
+    .setVersion('1.0.1')
+    .addTag('starter')
+    .build();
 
-  if (process.env.NODE_ENV === 'development') {
-    const swaggerConfig = new DocumentBuilder()
-      .setTitle('Unistory nestJS starter')
-      .setDescription('Unistory nestJS starter API')
-      .setVersion('1.0.1')
-      .addTag('starter')
-      .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
-    const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup('docs', app, document);
-  }
-
-  app.setGlobalPrefix('api', { exclude: [{ path: 'docs', method: RequestMethod.GET }] });
   app.enableCors();
 
   await app.listen(port || 3000);
