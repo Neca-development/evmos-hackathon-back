63cf97034e7cbedca52faccb5d669cdac863f422
Aleksey <klimenkoaleksey02@gmail.com>
2022-08-26 17:53:40 +0300

    feat: add get user info endpoint

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -4,6 +4,7 @@ import {
 } from '@nestjs/common'
 
 import { UniDecorators } from '@unistory/route-decorators'
+import { UserEntity } from 'src/data/entity/user.entiry'
 
 import { UserService } from 'src/service/user.service'
 import { DaoEntity } from '../data/entity/dao.entity'
@@ -15,10 +16,22 @@ export class UserController {
   @UniDecorators.Get(
     '/get-daos/:userAddress',
     'Get all daos for user',
-    false
+    false,
+    DaoEntity
   )
   async getDaos(@Param('userAddress') address: string): Promise<DaoEntity[]> {
-    const res = this.userService.getAllDaosByUserAddress(address)
+    const res = await this.userService.getAllDaosByUserAddress(address)
+    return res
+  }
+
+  @UniDecorators.Get(
+    '/:userAddress',
+    'Get all daos for user',
+    false,
+    UserEntity
+  )
+  async getUser(@Param('userAddress') address: string): Promise<UserEntity> {
+    const res = await this.userService.getByAddress(address)
     return res
   }
 }
